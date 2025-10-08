936652315c29f01c0b845d52cbf5b764c2d62309
serdchef <a.serdarcarl@gmail.com>
2025-10-08 21:35:20 +0300

    feat: add google-site-verification meta for Search Console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
+        {/* Google Search Console verification */}
+        <meta name="google-site-verification" content="pbY1q4F1-KfSpxOZ-ANTgFUTv-DrMk2ru26YKRKEmQ0" />
         <meta name="description" content="Global Shapers İzmir Hub - Gençlerin geleceği şekillendirdiği topluluk" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
