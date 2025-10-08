cfb2abe455a9cf468bbf1a5978fe3ab155616759
serdchef <a.serdarcarl@gmail.com>
2025-10-08 22:00:17 +0300

    chore: set favicon to Global Shapers İzmir logo (png) and add apple-touch-icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,12 @@ export default function Document() {
       <Head>
         {/* Google Search Console verification */}
         <meta name="google-site-verification" content="pbY1q4F1-KfSpxOZ-ANTgFUTv-DrMk2ru26YKRKEmQ0" />
-        <meta name="description" content="Global Shapers İzmir Hub - Gençlerin geleceği şekillendirdiği topluluk" />
-        <link rel="icon" href="/favicon.ico" />
+  <meta name="description" content="Global Shapers İzmir Hub - Gençlerin geleceği şekillendirdiği topluluk" />
+  {/* Prefer PNG logo as favicon (modern browsers) and keep .ico fallback */}
+  <link rel="icon" type="image/png" sizes="32x32" href="/images/gs-logo.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/images/gs-logo.png" />
+  <link rel="apple-touch-icon" href="/images/gs-logo.png" />
+  <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
