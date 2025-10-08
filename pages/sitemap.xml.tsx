58e2778cc99c4241b8714e3ccd2073e8e9bea1b8
serdchef <a.serdarcarl@gmail.com>
2025-10-08 21:50:00 +0300

    feat: add dynamic sitemap at /sitemap.xml from modulesData

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,45 @@
+import { GetServerSideProps } from 'next'
+import { modulesData } from '@/utils/modulesData'
+
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://glabalshapersizmir-us6d.vercel.app'
+
+function urlEntry(loc: string, lastmod = new Date().toISOString()) {
+  return `
+  <url>
+    <loc>${SITE_URL.replace(/\/$/, '')}${loc}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>`
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const staticPages = [
+    '/',
+    '/hakkimizda',
+    '/uyeler',
+    '/projeler',
+    '/iletisim',
+    '/mindcraft-pages/playground',
+    '/mindcraft-pages/gallery',
+  ]
+
+  const moduleUrls = (modulesData || []).map((m) => `/mindcraft-pages/modules/${m.id}`)
+  const urls = [...staticPages, ...moduleUrls]
+
+  const lastmod = new Date().toISOString()
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls.map((u) => urlEntry(u, lastmod)).join('\n')}
+  </urlset>`
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(xml)
+  res.end()
+
+  return { props: {} }
+}
+
+export default function Sitemap() {
+  return null
+}
