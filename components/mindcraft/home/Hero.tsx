31c8d78caac30fcddc62b632a6aa782b699636af
serdchef <a.serdarcarl@gmail.com>
2025-10-10 20:33:52 +0300

    style(hero): nudge hero content up to compensate for navbar (-mt-8)

--- components/mindcraft/home/Hero.tsx
+++ components/mindcraft/home/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
       </div>
 
       {/* Content */}
-  <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 text-center">
+  <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 text-center -mt-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
