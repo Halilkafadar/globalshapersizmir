528160ffab382bd302db2cd7d6840cae4fb5f3d5
serdchef <a.serdarcarl@gmail.com>
2025-10-14 04:05:33 +0300

    chore: remove committed build artifacts (.next) and backend venv; update .gitignore

--- data/projects.ts
+++ data/projects.ts
@@ -127,7 +127,7 @@ Our program has enabled participants to act 85% more consciously in their financ
   documentation: "/projects/financial-literacy/curriculum"
     },
     impact: {
-      reach: "2,000+ participants",
+      reach: "500+ participants",
       beneficiaries: "50+ schools",
       duration: "2023-Ongoing"
     }
